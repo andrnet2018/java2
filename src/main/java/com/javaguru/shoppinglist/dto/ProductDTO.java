6d72842fb1368f777c54f44c95c36028010f3b03
andrnet <andris.plotka@gmail.com>
2019-03-31 17:14:20 +0300

    Spring Web

---
+++ src/main/java/com/javaguru/shoppinglist/dto/ProductDTO.java
@@ -0,0 +1,74 @@
+package com.javaguru.shoppinglist.dto;
+
+import java.math.BigDecimal;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String category;
+    private BigDecimal discount;
+    private String description;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, BigDecimal price, String category,
+                      BigDecimal discount, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.discount = discount;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
